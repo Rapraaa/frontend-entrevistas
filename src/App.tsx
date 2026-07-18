c799d5824ce6262b86501f7944e161b50600e96c
Rapraaa <rahiruilova33@gmail.com>
2026-07-18 18:29:08 -0500

    feat: rutas del area publica, privada y protegidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ProtectedRoute } from './auth/ProtectedRoute';
+import { DashboardLayout } from './layouts/DashboardLayout';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Registro } from './pages/Registro';
@@ -7,6 +8,10 @@ import { DashboardPage } from './pages/DashboardPage';
 import { SetupPage } from './pages/SetupPage';
 import { InterviewPage } from './pages/InterviewPage';
 import { ResultPage } from './pages/ResultPage';
+import { Profile } from './pages/Profile';
+import { CatalogPage } from './pages/admin/CatalogPage';
+import { PublicCatalog } from './pages/PublicCatalog';
+import { PublicCatalogDetail } from './pages/PublicCatalogDetail';
 
 function App() {
   return (
@@ -15,14 +20,21 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/publico/:slug" element={<PublicCatalog />} />
+        <Route path="/publico/:slug/:id" element={<PublicCatalogDetail />} />
+
         <Route
-          path="/dashboard"
           element={
             <ProtectedRoute>
-              <DashboardPage />
+              <DashboardLayout />
             </ProtectedRoute>
           }
-        />
+        >
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/perfil" element={<Profile />} />
+          <Route path="/admin/catalogos/:slug" element={<CatalogPage />} />
+        </Route>
+
         <Route
           path="/setup"
           element={
